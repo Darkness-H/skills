e62bbb57e5ce472e938938ed487548c3a6858433
Darkness <1031439605@qq.com>
2024-12-29 15:18:31 +0100

    app.js -> user.controller.js and user.routes.js

--- app.js
+++ app.js
@@ -66,127 +66,9 @@ app.use(passport.session());
 app.use('/users', usersRouter);
 app.use('/skills', skillsRouter);
 
-
-/////////////////////
-/// Autenticación ///
-/////////////////////
-
-// Ruta por defecto que redirige a '/login'
+// Ruta por defecto que redirige a '/users/login'
 app.get('/', (req, res) => {
-  res.redirect('/login');
-});
-
-// Serve login page: GET /users/login
-app.get('/login/:message?', (req, res) => {
-    const message = req.query.message;
-    res.render('login', {
-        success_msg: message || null,
-        error_msg: null,
-        error: null,
-        user: null
-    });
-});
-
-// Serve register page: GET /users/register
-app.get('/register/:message?', (req, res) => {
-    const message = req.query.message;
-    res.render('register', {
-        success_msg: message || null,
-        error_msg: null,
-        error: null,
-    });
-});
-
-// Register endpoint: POST /users/register
-app.post('/register', async (req, res) => {
-    try {
-        const { username, password, password2 } = req.body;
-
-        // Se deben llenar todos los campos
-        if (!username || !password || !password2) {
-            console.log('Missing username or password');
-            return res.render('register', {success_msg: null, error_msg: 'Missing username or password', error: null});
-            //return res.status(400).send('Username and password are required');
-        }
-
-        // Check if the passwords are the same (Las contraseñas deben coincidir)
-        if (password !== password2) {
-            console.log('Both passwords must be the same!');
-            return res.render('register', {success_msg: null, error_msg: 'Both passwords must be the same!', error: null});
-            //return res.status(400).send('Both passwords must be the same!');
-        }
-
-        // Las contraseñas deben tener al menos 6 caracteres
-        if (password.length < 6) {
-            console.log('The password must contain at least 6 characters!');
-            return res.render('register', {success_msg: null, error_msg: 'The password must contain at least 6 characters!', error: null});
-            //return res.status(400).send('The password must contain at least 6 characters!');
-        }
-
-        // Check if user exists (El nombre de usuario debe ser único)
-        const existingUser = await User.findOne({ username });
-        if (existingUser) {
-            console.log('Username already exists:', username);
-            return res.render('register', {success_msg: null, error_msg: 'Username already exists', error: null});
-            //return res.status(400).send('Username already exists');
-        }
-
-        // Create new user
-        const user = new User({ username, password });
-
-        // Establecemos su rol como admin si es el primer usuario
-        const userCount = await User.countDocuments();
-        if (userCount === 0) {
-            user.admin = true;
-        }
-
-        try {
-            await user.save();
-            console.log('User registered successfully:', username);
-            res.redirect('/login?message=User registered successfully');
-        } catch (saveError) {
-            console.error('Error saving user:', saveError);
-            return res.render('register', {success_msg: null, error_msg: null, error: 'Error saving user'});
-            //res.status(500).send('Error saving user');
-        }
-    } catch (error) {
-      console.error('Registration error:', error);
-      return res.render('register', {success_msg: null, error_msg: null, error: 'Error registering user'});
-      //res.status(500).send('Error registering user');
-    }
-})
-
-// Login endpoint: POST /users/login
-app.post('/login', async (req, res) => {
-    try {
-        const { username, password } = req.body;
-
-        // Find user
-        const user = await User.findOne({ username });
-        if (!user) {
-            console.log('User not found');
-            return res.render('login', {success_msg: null, error_msg: 'User not found', error: null});
-            //return res.status(400).send('User not found');
-        }
-
-        // Check password
-        const validPassword = await bcrypt.compare(password, user.password);
-        if (!validPassword) {
-            console.log('Invalid password');
-            return res.render('login', {success_msg: null, error_msg: 'Invalid password', error: null});
-            //return res.status(400).send('Invalid password');
-        }
-
-        // Set session
-        //req.session.user = username;
-        req.session.user = user;
-        // Redirecionar a la página principal
-        res.redirect('/system');
-    } catch (error) {
-        console.log('Error logging in');
-        return res.render('login', {success_msg: null, error_msg: null, error: 'Error logging in'});
-        //res.status(500).send('Error logging in');
-    }
+  res.redirect('/users/login');
 });
 
 // Endpoint de la página principal (protected)
@@ -194,34 +76,9 @@ app.get('/system', (req, res) => {
     res.redirect('/skills');
 });
 
-// Endpoint de logout: GET /users/logout
-app.get('/logout', (req, res) => {
-    // Auntenticación
-    if (!req.session.user) {
-        // Redireccionar al Login
-        return res.redirect('/');
-    }
-
-    // Destruir la sesión y redirigir a la página de Login
-    req.session.destroy();
-    res.redirect('/login?message=Logged out successfully');
-})
-
-// Endpoint para visualizar la clasificación de usuarios: GET /users/leaderboard
-app.get('/leaderboard', (req, res) => {
-    // Auntenticación
-    if (!req.session.user) {
-        // Redireccionar al Login
-        return res.redirect('/');
-    }
-
-    // Recalcular la puntuación de todos los usuarios
-    // Ordena en relación a la puntuación
-    // Asigna insignias en función de puntuación
-    // Resultado: redirecciona a la vista leaderboard.ejs con la clasificación
-    // res.render('leaderboard');
-    res.render('/')
-})
+//////////////////////////
+// Authenticación OAuth //
+//////////////////////////
 
 // Ruta para iniciar la autenticación con GitHub
 app.get('/auth/github', passport.authenticate('github'));
@@ -234,6 +91,9 @@ app.get('/auth/github/callback',
         res.redirect('/system');
     }
 );
+
+//////////////////////////
+//////////////////////////
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,6 +2,22 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
 
-router.get('/', userController.getUsers);
+// GET /users/register
+router.get('/register/:message?', userController.showRegisterPage);
+
+// POST /users/register
+router.post('/register', userController.addNewUser);
+
+// GET /users/login
+router.get('/login/:message?', userController.showLoginPage);
+
+// POST /users/login
+router.post('/login', userController.loginSystem);
+
+// GET /users/logout
+router.get('/logout', userController.logoutSystem);
+
+// GET /users/leaderboard
+router.get('/leaderboard', userController.visualizeLeaderboard);
 
 module.exports = router;
